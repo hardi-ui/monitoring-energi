e73c11be0c2e8017b45494dff2281e2621410e43
Hardi <m.hardianur@gmail.com>
2026-01-31 19:09:33 +0800

    Siapkan auto admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,20 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Buat User Admin Otomatis
+        User::create([
+            'name' => 'Administrator',
+            'username' => 'admin',
+            'email' => 'admin@pkt.com',
+            'role' => 'admin',
+            'password' => Hash::make('password123'),
+        ]);
     }
 }
